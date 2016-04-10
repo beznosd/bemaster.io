41f4d1e2b7637ceaca9cc51c3c6e9959ed2b53fe
Dima <beznos.d@gmail.com>
2016-04-10 14:01:38 +0300

    Added animate.css package and defined animate.css jQuery function in the Meteor startup

--- client/index.jsx
+++ client/index.jsx
@@ -5,6 +5,15 @@ import { render } from 'react-dom';
 import TimerButton from '../imports/ui/TimerButton.jsx';
 
 Meteor.startup(() => {
+	$.fn.extend({
+	    animateCss: function (animationName) {
+	        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+	            $(this).removeClass('animated ' + animationName);
+	        });
+	    }
+	});
+	
 	render(<TimerButton />, document.getElementById('timer-button-block'));
 });
 
--- imports/ui/TimerButton.jsx
+++ imports/ui/TimerButton.jsx
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 export default class TimerButton extends Component {
 
+	toggleTimer() {
+		// $('i.large').fadeOut(200);
+		$('i.large').animateCss('bounce');
+	}
+
 	render() {
 		return (
-			<span className="timer-button">
+			<span onClick={this.toggleTimer.bind(this)} className="timer-button">
 				<i className="large material-icons">play_arrow</i>
 			</span>
 		);
