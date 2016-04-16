b99fe32e7223d9d2617f290e1043875d6809a650
Dima <beznos.d@gmail.com>
2016-04-16 21:39:01 +0300

    Started work on timer reactivity, now works good only for one side changes from one client to another but not vice versa, hard work is going ^_^

--- imports/ui/TimerButton.jsx
+++ imports/ui/TimerButton.jsx
@@ -1,6 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { createContainer } from 'meteor/react-meteor-data';
 
-export default class TimerButton extends Component {
+import classnames from 'classnames';
+
+import { TimerTime } from '../api/TimerTime.js';
+
+class TimerButton extends Component {
 
 	constructor(props) {
 		super(props);
@@ -23,7 +29,7 @@ export default class TimerButton extends Component {
 
 		if ( this.state.ticking ) {
 
-			i = setInterval(function(){
+			i = Meteor.setInterval(function(){
 				if( this.state.s < 59 ) {
 					let s = this.state.s + 1;
 					this.setState({s: s});
@@ -37,65 +43,175 @@ export default class TimerButton extends Component {
 				if (this.state.h >= 1) {
 					this.minimizeNumsFont();
 				}
+
+				Meteor.call('timerTime.insert', this.state.s, this.state.m, this.state.h, this.state.ticking);
+
 			}.bind(this), 1000);
 
 		} else {
-			clearInterval(i);
-		}
+			Meteor.call('timerTime.insert', this.state.s, this.state.m, this.state.h, this.state.ticking);
 
+			Meteor.clearInterval(i);
+		}
+		
 	}
 
 	// run function timer() and show/hide neccessary elements
 	toggleTimer() {
-		let timerNums = this.refs['timerNums'];
-		let timerStartArrow = this.refs['timerStartArrow'];
-		let timerPauseTime = this.refs['timerPauseTime'];
 
 		if (this.state.ticking === false) {
-
-			$(timerStartArrow).fadeOut(150);
-			$(timerPauseTime).fadeOut(150);
-
-			setTimeout(function() {
-				// $('.timer-nums').show().animateCss('zoomIn');
-				// this.refs['timerNums'].show().animateCss('zoomIn');
-				$(timerNums).show().animateCss('zoomIn');
-			}, 150);
+			this.startTimerView();
+			// update state with data from DB before start timer
+			this.updateTimeStates();
 
 			this.state.ticking = true;
-
 		} else {
-
-			// $('.timer-nums').fadeOut(150);
-			$(timerNums).fadeOut(150);
-			setTimeout(function() {
-				$(timerStartArrow).show().animateCss('zoomIn');
-				$(timerPauseTime).show().animateCss('zoomIn');
-			}, 150);
+			this.pauseTimerView();
 
 			this.state.ticking = false;
-
 		}
 
 		this.timer();
 	}
 
+	startTimerView() {
+		let timerNums = this.refs['timerNums'];
+		let timerStartArrow = this.refs['timerStartArrow'];
+		let timerPauseTime = this.refs['timerPauseTime'];
+
+		$(timerStartArrow).fadeOut(150);
+		$(timerPauseTime).fadeOut(150);
+
+		setTimeout(function() {
+			$(timerNums).show().animateCss('zoomIn');
+		}, 150);
+	}
+
+	pauseTimerView() {
+		let timerNums = this.refs['timerNums'];
+		let timerStartArrow = this.refs['timerStartArrow'];
+		let timerPauseTime = this.refs['timerPauseTime'];
+
+		$(timerNums).fadeOut(150);
+		
+		setTimeout(function() {
+			$(timerStartArrow).show().animateCss('zoomIn');
+			$(timerPauseTime).show().animateCss('zoomIn');
+		}, 150);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		// console.log(this.state.ticking);
+		this.setState({ticking: nextProps.timerTime[0].ticking});
+		// console.log(this.state.ticking);
+		// console.log(nextProps);
+	}
+
+
+	renderTime() {
+		// at first launch render time from DB, then when timer will work will, work this
+		if ( this.props.timerTime[0] ) {
+			let seconds = this.props.timerTime[0].seconds;
+			let minutes = this.props.timerTime[0].minutes;
+			let hours = this.props.timerTime[0].hours;
+			return (
+				<span>{hours ? hours + ':' : ''}
+				{hours ? ( (minutes < 10) ? '0' + minutes + ':' : minutes + ':' ) : ( minutes ? minutes + ':' : '' )}
+				{(seconds < 10) ? '0'+seconds : seconds}</span>
+			);
+		// at first launch render initial time 0:0:0
+		} else {
+			return (
+				<span>{this.state.h ? this.state.h + ':' : ''}
+				{this.state.h ? ( (this.state.m < 10) ? '0' + this.state.m + ':' : this.state.m + ':' ) : ( this.state.m ? this.state.m + ':' : '' )}
+				{(this.state.s < 10) ? '0'+this.state.s : this.state.s}</span>
+			);
+		}
+	}
+
+	// Update time states to recently logged time if it exists in db 
+	updateTimeStates() {
+		if ( this.props.timerTime[0] ) {
+			if ( this.props.timerTime[0].seconds ) {
+				this.setState({ s: this.props.timerTime[0].seconds });
+			}
+			if ( this.props.timerTime[0].minutes ) {
+				this.setState({ m: this.props.timerTime[0].minutes });
+			}
+			if ( this.props.timerTime[0].hours ) {
+				this.setState({ h: this.props.timerTime[0].hours });
+			}
+			if ( this.props.timerTime[0].ticking ) {
+				this.setState({ticking: this.props.timerTime[0].ticking});
+			}
+		}
+	}
+
 	render() {
-		return (
-			<span onClick={this.toggleTimer.bind(this)} className="timer">
-				<span ref="timerNums" className="timer-nums">
-					{this.state.h ? this.state.h + ':' : ''}
-					{this.state.h ? ( (this.state.m < 10) ? '0' + this.state.m + ':' : this.state.m + ':' ) : ( this.state.m ? this.state.m + ':' : '' )}
-					{(this.state.s < 10) ? '0'+this.state.s : this.state.s}
+		if (this.props.timerTime[0]) {
+
+			// console.log(this.props.timerTime[0].ticking);
+
+			const timerNumsClasses = classnames({
+				hidden: !this.props.timerTime[0].ticking,
+				'display-block': this.props.timerTime[0].ticking,
+				'timer-nums': true
+			});
+			const timerPauseTimeClasses = classnames({
+				hidden: this.props.timerTime[0].ticking,
+				'display-block': !this.props.timerTime[0].ticking,
+				'timer-pause_time': true
+			});
+			const timerStartArrowClasses = classnames({
+				hidden: this.props.timerTime[0].ticking,
+				'display-block': !this.props.timerTime[0].ticking,
+				'timer-start_arrow': true,
+			});
+
+			return (
+				<span onClick={this.toggleTimer.bind(this)} className="timer">
+					<span ref="timerNums" className={timerNumsClasses}>
+						{this.renderTime()}
+					</span>
+					<div ref="timerStartArrow" className={timerStartArrowClasses}>
+						<i className="triangle"></i>
+					</div>
+					<span ref="timerPauseTime" className={timerPauseTimeClasses}>
+						{this.renderTime()}
+					</span>
 				</span>
-				<i ref="timerStartArrow" className="timer-start_arrow large material-icons">play_arrow</i>
-				<span ref="timerPauseTime" className="timer-pause_time">
-					{this.state.h ? this.state.h + ':' : ''}
-					{this.state.h ? ( (this.state.m < 10) ? '0' + this.state.m + ':' : this.state.m + ':' ) : ( this.state.m ? this.state.m + ':' : '' )}
-					{(this.state.s < 10) ? '0'+this.state.s : this.state.s}
+			);
+
+		} else {
+
+			return (
+				<span onClick={this.toggleTimer.bind(this)} className="timer">
+					<span ref="timerNums" className='timer-nums hidden'>
+						{this.renderTime()}
+					</span>
+					<div ref="timerStartArrow" className="timer-start_arrow">
+						<i className="triangle"></i>
+					</div>
+					<span ref="timerPauseTime" className='timer-pause_time hidden'>
+						{this.renderTime()}
+					</span>
 				</span>
-			</span>
-		);
+			);
+
+		}
 	}
 
 }
+
+TimerButton.PropTypes = {
+	timerTime: PropTypes.array.isRequired
+}
+
+export default createContainer(() => {
+	Meteor.subscribe('timerTime');
+
+	return {
+		timerTime: TimerTime.find({userId: 1}).fetch(),
+		// ticking: TimerTime.find({userId: 1}, {fields: {ticking: 1}}).fetch()
+	};
+}, TimerButton);
