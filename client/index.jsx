71f5011d966fb874cac359db03455923ef157241
Dima <beznos.d@gmail.com>
2016-05-04 11:43:03 +0300

    Removed node modules and added package.json and .gitignore for node_modules and the root of project, here is only timer and nothing else, now will try to install node package but will not to track them in the next commit, because .gitignore is already exist

--- client/index.jsx
+++ client/index.jsx
@@ -3,19 +3,10 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import '../imports/startup/routes.jsx';
 
-// import MainLayout from '../imports/ui/MainLayout.jsx';
+import TimerButton from '../imports/ui/TimerButton.jsx';
 
 Meteor.startup(() => {
-	$.fn.extend({
-	    animateCss: function (animationName) {
-	        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-	        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
-	            $(this).removeClass('animated ' + animationName);
-	        });
-	    }
-	});
-
-	// render(<TimerButton />, document.getElementById('timer-block'));
+	render(<TimerButton />, document.getElementById('TimerButton'));
 });
 
 
